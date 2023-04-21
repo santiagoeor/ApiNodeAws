f48068cf7c6cb82863dadb724cc3918cf3b8ee80
Santiago Orozco <everardoorozcocarmona@gmail.com>
2023-04-20 21:32:14 -0500

    cuadrando elserver pa aws

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const app = express()
-const http = require('http');
 const port = 3000
-// const ip = '192.168.18.11';
-const ip = '3.17.159.171';
 
-const serverHttp = http.createServer(app)
-serverHttp.listen(port, ip);
-serverHttp.on('listening', () => console.info(`Notes App running at http://${ip}:${port}`));
 
 app.get('/saludo', (req, res) => {
   res.send('Hello World!')
